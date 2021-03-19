aa462dda78726942786fe88f26ed3bf25a3f1c6e
Magomed Gadzhiev <colossus130600@yandex.ru>
2021-03-19 22:24:12 +0300

    Fix styles

--- src/components/SurveysPage/NoSurveyInstruction.js
+++ src/components/SurveysPage/NoSurveyInstruction.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  noSurveyInstruction: {
+    height: "calc(100vh - 160px)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+}));
 
 function NoSurveyInstruction() {
+  const classes = useStyles();
   return (
-    <div className="no-survey-instruction">
+    <div className={classes.noSurveyInstruction}>
       <Typography variant="h5" align="center">
         Нажмите на опрос из списка опросов или создайте новый
       </Typography>
--- src/components/SurveysPage/SurveysPageAppBar.js
+++ src/components/SurveysPage/SurveysPageAppBar.js
@@ -8,10 +8,19 @@ import {
 } from "@material-ui/core";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import { useAuth } from "./../../contexts/AuthContext";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  toolbarLinks: {
+    display: "flex",
+    justifyContent: "flex-end",
+    flex: "1 1 100%"
+  }
+}));
 
 function SurveysPageAppBar(props) {
   const [appBarMenuAnchorEl, setAppBarMenuAnchorEl] = useState(null);
-
+  const classes = useStyles();
   const { logOut, currentUser } = useAuth();
 
   const openAppBarMenu = (e) => {
@@ -25,7 +34,7 @@ function SurveysPageAppBar(props) {
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <div className="toolbar-links">
+          <div className={classes.toolbarLinks}>
             <Button onClick={openAppBarMenu} color="inherit" startIcon={<AccountCircle />}>
               {currentUser.name}
             </Button>
