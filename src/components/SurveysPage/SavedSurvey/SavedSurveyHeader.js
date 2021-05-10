5b1206cc9b27d50a4d8929e0c51fb9c137b858f7
Magomed <colossus130600@yandex.ru>
2021-05-10 11:49:28 +0300

    Add <Paper/> and <Grid/> components

--- src/components/SurveysPage/SavedSurvey/SavedSurveyHeader.js
+++ src/components/SurveysPage/SavedSurvey/SavedSurveyHeader.js
@@ -1,65 +1,71 @@
 import React from "react";
-import { Grid, Button, Tabs, Tab } from "@material-ui/core";
+import { Grid, Button, Tabs, Tab, Paper } from "@material-ui/core";
 import LinkIcon from "@material-ui/icons/Link";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-function SavedSurveyHeader({tabValue, changeTab, openGetLinkDialog}) {
+function SavedSurveyHeader({ tabValue, changeTab, openGetLinkDialog }) {
   const isSmBreakpoint = useMediaQuery("(max-width: 600px)");
   return (
     <>
-      {isSmBreakpoint ? (
-        <>
-          <Grid item xs={12}>
-            <Tabs
-              value={tabValue}
-              onChange={changeTab}
-              indicatorColor="primary"
-              textColor="primary"
-              variant="fullWidth"
-              aria-label="full width tabs example">
-              <Tab value={0} label="Вопросы" />
-              <Tab value={1} label="Ответы" />
-            </Tabs>
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              fullWidth
-              variant="contained"
-              color="default"
-              startIcon={<LinkIcon />}
-              size="small"
-              onClick={openGetLinkDialog}>
-              ссылка на опрос
-            </Button>
-          </Grid>
-        </>
-      ) : (
-        <Grid item container xs={12} justify="space-between" alignItems="center">
-          <Grid item md={7}>
-            <Tabs
-              value={tabValue}
-              onChange={changeTab}
-              indicatorColor="primary"
-              textColor="primary"
-              variant="fullWidth"
-              aria-label="full width tabs example">
-              <Tab value={0} label="Вопросы" />
-              <Tab value={1} label="Ответы" />
-            </Tabs>
-          </Grid>
-          <Grid item md={4}>
-            <Button
-              fullWidth
-              variant="contained"
-              color="default"
-              startIcon={<LinkIcon />}
-              size="small"
-              onClick={openGetLinkDialog}>
-              ссылка на опрос
-            </Button>
-          </Grid>
-        </Grid>
-      )}
+      <Grid item xs={12}>
+        <Paper elevation={3} className="paper">
+          {isSmBreakpoint ? (
+            <>
+              <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  <Tabs
+                    value={tabValue}
+                    onChange={changeTab}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                    aria-label="full width tabs example">
+                    <Tab value={0} label="Вопросы" />
+                    <Tab value={1} label="Ответы" />
+                  </Tabs>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    fullWidth
+                    variant="contained"
+                    color="default"
+                    startIcon={<LinkIcon />}
+                    size="small"
+                    onClick={openGetLinkDialog}>
+                    ссылка на опрос
+                  </Button>
+                </Grid>
+              </Grid>
+            </>
+          ) : (
+            <Grid item container xs={12} justify="space-between" alignItems="center">
+              <Grid item md={7}>
+                <Tabs
+                  value={tabValue}
+                  onChange={changeTab}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  variant="fullWidth"
+                  aria-label="full width tabs example">
+                  <Tab value={0} label="Вопросы" />
+                  <Tab value={1} label="Ответы" />
+                </Tabs>
+              </Grid>
+              <Grid item md={4}>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="default"
+                  startIcon={<LinkIcon />}
+                  size="small"
+                  onClick={openGetLinkDialog}>
+                  ссылка на опрос
+                </Button>
+              </Grid>
+            </Grid>
+          )}
+        </Paper>
+      </Grid>
     </>
   );
 }
