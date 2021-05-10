cdada1b3e88bdbe425ce869bf094d8fed77c85a7
Magomed <colossus130600@yandex.ru>
2021-05-10 11:42:56 +0300

    Change title and note inputs and <Grid/> and <Paper/> components

--- src/components/SurveysPage/SavedSurvey/SavedSurveyQuestionsTab/SavedSurveyQuestionsTab.js
+++ src/components/SurveysPage/SavedSurvey/SavedSurveyQuestionsTab/SavedSurveyQuestionsTab.js
@@ -1,16 +1,45 @@
 import React from "react";
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import SavedSurveyElements from "../SavedSurveyElements";
 import TabPanel from "../TabPanel";
 
+const useStyles = makeStyles((theme) => ({
+  titleInput: {
+    fontSize: "36px",
+    fontWeight: 700,
+  },
+  noteInput: {
+    fontSize: "20px",
+  },
+}));
+
 function SavedSurveyQuestionsTab({ tabValue, surveyData }) {
+  const classes = useStyles();
   return (
     <TabPanel value={tabValue} index={0}>
       <Grid item xs={12}>
-        <TextField fullWidth label="Название опроса" variant="outlined" size="small" value={surveyData.title} />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField fullWidth multiline label="Примечание" variant="outlined" size="small" value={surveyData.note} />
+        <Paper elevation={3} className="paper">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                placeholder="Название опроса"
+                value={surveyData.title}
+                InputProps={{ className: classes.titleInput }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                placeholder="Примечание"
+                multiline
+                value={surveyData.note}
+                InputProps={{ className: classes.noteInput }}
+              />
+            </Grid>
+          </Grid>
+        </Paper>
       </Grid>
       <SavedSurveyElements surveyElementsData={surveyData.elements} />
     </TabPanel>
